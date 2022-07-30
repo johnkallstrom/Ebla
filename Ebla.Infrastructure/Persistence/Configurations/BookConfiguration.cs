e1398c2644998f2d1823a697e93f32ba68353f14
John Källström <jekallstrom@gmail.com>
2022-07-30 21:23:38 +0200

    Add entity configurations for book and genre

--- Ebla.Infrastructure/Persistence/Configurations/BookConfiguration.cs
+++ Ebla.Infrastructure/Persistence/Configurations/BookConfiguration.cs
@@ -13,6 +13,44 @@ namespace Ebla.Infrastructure.Persistence.Configurations
             builder.Property(x => x.Pages).HasColumnName("Pages");
             builder.Property(x => x.Published).HasColumnName("Published");
             builder.Property(x => x.IsReserved).HasColumnName("IsReserved");
+            builder.Property(x => x.AuthorId).HasColumnName("AuthorId");
+            builder.Property(x => x.GenreId).HasColumnName("GenreId");
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "Rendezvous with Rama",
+                    Pages = 256,
+                    Published = new DateTime(1973, 6, 1),
+                    IsReserved = false,
+                    AuthorId = 1,
+                    GenreId = 1
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "Foundation",
+                    Pages = 255,
+                    Published = new DateTime(1942, 5, 1),
+                    IsReserved = false,
+                    AuthorId = 2,
+                    GenreId = 1
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "The Left Hand of Darkness",
+                    Pages = 286,
+                    Published = new DateTime(1969, 3, 1),
+                    IsReserved = false,
+                    AuthorId = 3,
+                    GenreId = 1
+                },
+            };
+
+            builder.HasData(books);
         }
     }
 }
---
+++ Ebla.Infrastructure/Persistence/Configurations/GenreConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ebla.Infrastructure.Persistence.Configurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.ToTable("Genre");
+
+            builder.Property(x => x.Id).HasColumnName("Id");
+            builder.Property(x => x.Name).HasColumnName("Name");
+
+            var genres = new List<Genre>
+            {
+                new Genre
+                {
+                    Id = 1,
+                    Name = "Science Fiction"
+                },
+                new Genre
+                {
+                    Id = 2,
+                    Name = "Fantasy"
+                },
+                new Genre
+                {
+                    Id = 3,
+                    Name = "Horror"
+                },
+            };
+
+            builder.HasData(genres);
+        }
+    }
+}
