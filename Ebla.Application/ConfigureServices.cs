db5b4f45c972ea72d0ee307d5de402c426a043ea
John Källström <jekallstrom@gmail.com>
2023-02-17 14:21:42 +0100

    Add service configurations

---
+++ Ebla.Application/ConfigureServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ebla.Application
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- Ebla.Infrastructure/ConfigureServices.cs
+++ Ebla.Infrastructure/ConfigureServices.cs
@@ -3,12 +3,14 @@ using Ebla.Infrastructure.Persistence.Repositories;
 
 namespace Ebla.Infrastructure
 {
-    public static class InfrastructureServiceRegistration
+    public static class ConfigureServices
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<EblaDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             return services;
         }
