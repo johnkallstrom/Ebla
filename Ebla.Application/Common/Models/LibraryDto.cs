7778b5773c22c235e8da4b965a004aeb2ac746de
John Källström <johnkaellstrom@gmail.com>
2023-10-07 14:54:41 +0200

    Update librarydto

--- Ebla.Application/Common/Models/LibraryDto.cs
+++ Ebla.Application/Common/Models/LibraryDto.cs
@@ -4,10 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Address { get; set; }
-        public string Country { get; set; }
         public DateTime? Established { get; set; }
-        public string Website { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? LastModified { get; set; }
 
