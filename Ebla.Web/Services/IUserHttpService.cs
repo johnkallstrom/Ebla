67e350709f4d07a4c8a1eff23431a2dc7714a1c9
John Källström <johnkaellstrom@gmail.com>
2023-10-23 18:39:31 +0200

    Add user http service

---
+++ Ebla.Web/Services/IUserHttpService.cs
@@ -0,0 +1,7 @@
+﻿namespace Ebla.Web.Services
+{
+    public interface IUserHttpService
+    {
+        Task LoginUserAsync(string username, string password);
+    }
+}
---
+++ Ebla.Web/Services/UserHttpService.cs
@@ -0,0 +1,20 @@
+﻿namespace Ebla.Web.Services
+{
+    public class UserHttpService : IUserHttpService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserHttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task LoginUserAsync(string username, string password)
+        {
+            Console.WriteLine($"{username}, {password}");
+            Console.WriteLine(_httpClient.BaseAddress.ToString());
+
+            throw new NotImplementedException();
+        }
+    }
+}
