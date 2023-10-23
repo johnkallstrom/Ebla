769cc5bb17b62b6da5d29989d59a51d03567f3c9
John Källström <johnkaellstrom@gmail.com>
2023-10-23 11:57:37 +0200

    Validate login form

--- Ebla.Web/Models/LoginModel.cs
+++ Ebla.Web/Models/LoginModel.cs
@@ -2,7 +2,12 @@
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Please enter a username")]
+        [DataType(DataType.Text)]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
