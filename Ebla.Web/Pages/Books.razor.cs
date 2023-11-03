bd06dd8fd0b4e6872ab8af50fcf6237aa65a0782
John Källström <johnkaellstrom@gmail.com>
2023-11-03 13:48:22 +0100

    Add books page

---
+++ Ebla.Web/Pages/Books.razor.cs
@@ -0,0 +1,17 @@
+﻿using Ebla.Web.Services.Interfaces;
+
+namespace Ebla.Web.Pages
+{
+    public partial class Books
+    {
+        [Inject]
+        public IBookHttpService BookHttpService { get; set; }
+
+        public List<BookViewModel> BookList { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            BookList = await BookHttpService.GetAllAsync();
+        }
+    }
+}
