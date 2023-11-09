11b1ee21340807d93cdd9a7af68043c4443261d4
John Källström <johnkaellstrom@gmail.com>
2023-11-09 10:16:45 +0100

    Add is authorized check on user links in navbbar

---
+++ Ebla.Web/Shared/Navbar.razor.cs
@@ -0,0 +1,30 @@
+﻿namespace Ebla.Web.Shared
+{
+    public partial class Navbar
+    {
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public bool IsAuthorized { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            string token = await LocalStorage.GetItemAsStringAsync("token");
+            if (!string.IsNullOrEmpty(token))
+            {
+                IsAuthorized = true;
+            }
+
+            // Todo: also check if token has expired
+        }
+
+        public async Task SignOut()
+        {
+            await LocalStorage.RemoveItemAsync("token");
+            NavigationManager.NavigateTo(NavigationManager.BaseUri, true);
+        }
+    }
+}
