a1743bb1eb33852ef97094add2dce21e1a2f74af
John Källström <johnkaellstrom@gmail.com>
2023-11-09 14:34:58 +0100

    Add appsettings.json file to Web project

--- Ebla.Web/Program.cs
+++ Ebla.Web/Program.cs
@@ -3,9 +3,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient<IUserHttpService, UserHttpService>(client => client.BaseAddress = new Uri("http://localhost:5121"));
-builder.Services.AddHttpClient<IBookHttpService, BookHttpService>(client => client.BaseAddress = new Uri("http://localhost:5121"));
+var services = builder.Services;
+var configuration = builder.Configuration;
 
-builder.Services.AddBlazoredLocalStorage();
+services.AddHttpClient<IUserHttpService, UserHttpService>(client => client.BaseAddress = new Uri(configuration.GetValue<string>("Api:BaseUrl")));
+services.AddHttpClient<IBookHttpService, BookHttpService>(client => client.BaseAddress = new Uri(configuration.GetValue<string>("Api:BaseUrl")));
+services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
