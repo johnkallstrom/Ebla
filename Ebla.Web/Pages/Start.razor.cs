3c9e69e6cf1a3b0851a21b7f81a350f1480268f9
John Källström <johnkaellstrom@gmail.com>
2023-11-09 09:50:14 +0100

    Check if token exists / user authorized on startup

---
+++ Ebla.Web/Pages/Start.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace Ebla.Web.Pages
+{
+    public partial class Start
+    {
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
+
+        public bool IsAuthorized { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            string token = await LocalStorage.GetItemAsStringAsync("token");
+            if (!string.IsNullOrEmpty(token))
+            {
+                IsAuthorized = true;
+            }
+        }
+    }
+}
