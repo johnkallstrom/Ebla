270a253eebe41a5f06ad82a441ffc207366ec585
John Källström <johnkaellstrom@gmail.com>
2023-11-11 14:47:08 +0100

    Change

--- Ebla.Web/Pages/Start.razor.cs
+++ Ebla.Web/Pages/Start.razor.cs
@@ -2,22 +2,5 @@
 {
     public partial class Start
     {
-        [Inject]
-        public ILocalStorageService LocalStorage { get; set; }
-
-        public bool IsAuthorized { get; set; }
-        public string Token { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            string token = await LocalStorage.GetItemAsStringAsync("token");
-            if (!string.IsNullOrEmpty(token))
-            {
-                IsAuthorized = true;
-                Token = token;
-            }
-
-            // Todo: also check if token has expired
-        }
     }
 }
--- Ebla.Web/Shared/Navbar.razor.cs
+++ Ebla.Web/Shared/Navbar.razor.cs
@@ -8,19 +8,6 @@
         [Inject]
         public NavigationManager NavigationManager { get; set; }
 
-        public bool IsAuthorized { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            string token = await LocalStorage.GetItemAsStringAsync("token");
-            if (!string.IsNullOrEmpty(token))
-            {
-                IsAuthorized = true;
-            }
-
-            // Todo: also check if token has expired
-        }
-
         public async Task SignOut()
         {
             await LocalStorage.RemoveItemAsync("token");
