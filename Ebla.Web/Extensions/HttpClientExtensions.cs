534afffc0b3e063d06a4043d8644a7d035732e90
John Källström <johnkaellstrom@gmail.com>
2023-11-17 11:43:36 +0100

    Add http client extensions

---
+++ Ebla.Web/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Ebla.Web.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void SetAuthorizationHeaderWithToken(this HttpClient httpClient, string token)
+        {
+            if (!string.IsNullOrEmpty(token))
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
