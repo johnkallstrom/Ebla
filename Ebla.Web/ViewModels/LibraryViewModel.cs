d98e51d8c8d5e3c7f7e614234eb9d6ac36f43ee8
John Källström <johnkaellstrom@gmail.com>
2023-11-21 20:46:27 +0100

    Change

--- Ebla.Web/ViewModels/LibraryViewModel.cs
+++ Ebla.Web/ViewModels/LibraryViewModel.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Address { get; set; }
         public DateTime? Established { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? LastModified { get; set; }
