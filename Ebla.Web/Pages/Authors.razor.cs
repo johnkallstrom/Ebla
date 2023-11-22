48b71e96df641951b2336c05b519ce3f7617b5a1
John Källström <johnkaellstrom@gmail.com>
2023-11-22 10:18:55 +0100

    Display author list

--- Ebla.Web/Pages/Authors.razor.cs
+++ Ebla.Web/Pages/Authors.razor.cs
@@ -2,5 +2,31 @@
 {
     public partial class Authors
     {
+        [Inject]
+        public IHttpService HttpService { get; set; }
+
+        public List<AuthorViewModel> AuthorList { get; set; }
+        public List<string> Errors { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                var response = await HttpService.GetAsync("/api/authors");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    AuthorList = await response.Content.ReadFromJsonAsync<List<AuthorViewModel>>();
+                }
+                else
+                {
+                    Errors = new List<string> { await response.Content.ReadAsStringAsync() };
+                }
+            }
+            catch (Exception ex)
+            {
+                Errors.Add(ex.Message);
+            }
+        }
     }
 }
---
+++ Ebla.Web/ViewModels/AuthorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ebla.Web.ViewModels
+{
+    public class AuthorViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
