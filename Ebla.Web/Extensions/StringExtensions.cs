0b4a13d29383803de3d7a472731d06d481a85dd9
John Källström <johnkaellstrom@gmail.com>
2023-11-23 14:40:00 +0100

    Add datetime format extensions

---
+++ Ebla.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Ebla.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToCustomDateFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd MMMM yyyy");
+        }
+
+        public static string ToYear(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy");
+        }
+    }
+}
