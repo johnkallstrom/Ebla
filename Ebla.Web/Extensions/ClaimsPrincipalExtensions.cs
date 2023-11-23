8a50dc11ac5d8a20b45ecb53c38ecb927050cc54
John Källström <johnkaellstrom@gmail.com>
2023-11-23 12:13:00 +0100

    User extension method

---
+++ Ebla.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Ebla.Web.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetIdentifier(this ClaimsPrincipal user)
+        {
+            var id = user.Claims.FirstOrDefault(claim => claim.Type.Equals("nameid")).Value;
+
+            return Guid.Parse(id);
+        }
+    }
+}
