d5c647a6a69b29ec259b8156b2104a17b568652e
John Källström <johnkaellstrom@gmail.com>
2023-11-26 23:50:24 +0100

    Remove description property from library

--- Ebla.Domain/Entities/Library.cs
+++ Ebla.Domain/Entities/Library.cs
@@ -3,7 +3,6 @@
     public class Library : BaseEntity<int>
     {
         public string Name { get; set; }
-        public string Description { get; set; }
         public DateTime? Established { get; set; }
 
         public List<Book> Books { get; set; }
--- Ebla.Infrastructure/Persistence/EntityConfigurations/LibraryConfiguration.cs
+++ Ebla.Infrastructure/Persistence/EntityConfigurations/LibraryConfiguration.cs
@@ -8,7 +8,6 @@
 
             builder.Property(x => x.Id).HasColumnName("Id");
             builder.Property(x => x.Name).HasColumnName("Name");
-            builder.Property(x => x.Description).HasColumnName("Description");
             builder.Property(x => x.Established).HasColumnName("Established");
             builder.Property(x => x.CreatedOn).HasColumnName("CreatedOn");
             builder.Property(x => x.LastModified).HasColumnName("LastModified");
