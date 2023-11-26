2c6cf848e6013280e7cf0b37bd0f968a1ab6ba5d
John Källström <johnkaellstrom@gmail.com>
2023-11-26 23:49:56 +0100

    Add description property to genre

--- Ebla.Domain/Entities/Genre.cs
+++ Ebla.Domain/Entities/Genre.cs
@@ -3,6 +3,7 @@
     public class Genre : BaseEntity<int>
     {
         public string Name { get; set; }
+        public string Description { get; set; }
 
         public List<Book> Books { get; set; }
     }
--- Ebla.Infrastructure/Persistence/EntityConfigurations/GenreConfiguration.cs
+++ Ebla.Infrastructure/Persistence/EntityConfigurations/GenreConfiguration.cs
@@ -8,6 +8,7 @@
 
             builder.Property(x => x.Id).HasColumnName("Id");
             builder.Property(x => x.Name).HasColumnName("Name");
+            builder.Property(x => x.Description).HasColumnName("Description");
             builder.Property(x => x.CreatedOn).HasColumnName("CreatedOn");
             builder.Property(x => x.LastModified).HasColumnName("LastModified");
 
