51c5762bded02388eeb685b65da40a5b1e7611ee
John Källström <johnkaellstrom@gmail.com>
2023-11-26 23:32:04 +0100

    Refactor library entity

--- Ebla.Domain/Entities/Library.cs
+++ Ebla.Domain/Entities/Library.cs
@@ -4,7 +4,6 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Address { get; set; }
         public DateTime? Established { get; set; }
 
         public List<Book> Books { get; set; }
--- Ebla.Infrastructure/Persistence/EntityConfigurations/LibraryConfiguration.cs
+++ Ebla.Infrastructure/Persistence/EntityConfigurations/LibraryConfiguration.cs
@@ -9,7 +9,6 @@
             builder.Property(x => x.Id).HasColumnName("Id");
             builder.Property(x => x.Name).HasColumnName("Name");
             builder.Property(x => x.Description).HasColumnName("Description");
-            builder.Property(x => x.Address).HasColumnName("Address");
             builder.Property(x => x.Established).HasColumnName("Established");
             builder.Property(x => x.CreatedOn).HasColumnName("CreatedOn");
             builder.Property(x => x.LastModified).HasColumnName("LastModified");
