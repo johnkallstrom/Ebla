e2f96aa297d93204656cd4bc9c8373aac2e9d3d0
John Källström <johnkaellstrom@gmail.com>
2023-11-27 11:45:43 +0100

    Add new joining table between book and library

---
+++ Ebla.Domain/Entities/BookLibrary.cs
@@ -0,0 +1,8 @@
+﻿namespace Ebla.Domain.Entities
+{
+    public class BookLibrary : BaseEntity<int>
+    {
+        public int BookId { get; set; }
+        public int LibraryId { get; set; }
+    }
+}
