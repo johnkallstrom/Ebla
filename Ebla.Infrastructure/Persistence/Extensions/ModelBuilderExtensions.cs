f7acfb674b41db5ab1d55d3f1fc7f0c8dbffa47a
John Källström <johnkaellstrom@gmail.com>
2023-11-27 11:43:22 +0100

    Extension method for setting identity table names

---
+++ Ebla.Infrastructure/Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Ebla.Infrastructure.Persistence.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyIdentityConfigurations(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<ApplicationRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaim");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("UserToken");
+        }
+    }
+}
