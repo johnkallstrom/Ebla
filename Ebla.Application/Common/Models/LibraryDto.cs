17332c1a56f460330f5a1f11be7859873f449500
John Källström <johnkaellstrom@gmail.com>
2023-11-27 17:57:48 +0100

    Update library dtos

--- Ebla.Application/Common/Models/LibraryDto.cs
+++ Ebla.Application/Common/Models/LibraryDto.cs
@@ -4,13 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Address { get; set; }
         public DateTime? Established { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? LastModified { get; set; }
 
         public List<LibraryCardDto> LibraryCards { get; set; }
-        public List<BookSlimDto> Books { get; set; }
     }
 }
