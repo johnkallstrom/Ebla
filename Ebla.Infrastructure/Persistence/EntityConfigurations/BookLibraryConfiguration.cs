62f849951832dd595aa9061693ab34a09ef8737e
John Källström <johnkaellstrom@gmail.com>
2023-11-27 12:10:01 +0100

    Db change

---
+++ Ebla.Infrastructure/Persistence/EntityConfigurations/BookLibraryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Ebla.Infrastructure.Persistence.EntityConfigurations
+{
+    public class BookLibraryConfiguration : IEntityTypeConfiguration<BookLibrary>
+    {
+        public void Configure(EntityTypeBuilder<BookLibrary> builder)
+        {
+            builder.ToTable("BookLibrary");
+
+            builder.Property(x => x.Id).HasColumnName("Id");
+            builder.Property(x => x.BookId).HasColumnName("BookId");
+            builder.Property(x => x.LibraryId).HasColumnName("LibraryId");
+
+            var bookLibraryList = new List<BookLibrary>();
+
+            builder.HasData(bookLibraryList);
+        }
+    }
+}
