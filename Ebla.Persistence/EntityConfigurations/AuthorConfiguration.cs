9d87e945f6a20d4b83bcc3b15c7a819c8b90233b
John Källström <johnkaellstrom@gmail.com>
2023-11-28 13:55:41 +0100

    Update entity configs

--- Ebla.Persistence/EntityConfigurations/AuthorConfiguration.cs
+++ Ebla.Persistence/EntityConfigurations/AuthorConfiguration.cs
@@ -8,7 +8,11 @@
 
             builder.Property(x => x.Id).HasColumnName("Id");
             builder.Property(x => x.Name).HasMaxLength(200).HasColumnName("Name");
+            builder.Property(x => x.Description).HasColumnName("Description");
             builder.Property(x => x.Born).HasColumnName("Born");
+            builder.Property(x => x.Country).HasColumnName("Country");
+            builder.Property(x => x.ImageLink).HasColumnName("ImageLink");
+            builder.Property(x => x.Wikipedia).HasColumnName("Wikipedia");
             builder.Property(x => x.CreatedOn).HasColumnName("CreatedOn");
             builder.Property(x => x.LastModified).HasColumnName("LastModified");
 
