c5537292c4333bfb1bd1b8e70548c1442261ead5
John Källström <johnkaellstrom@gmail.com>
2023-11-28 13:53:56 +0100

    Rename and add property

--- Ebla.Domain/Entities/Author.cs
+++ Ebla.Domain/Entities/Author.cs
@@ -5,7 +5,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Born { get; set; }
-        public string Image { get; set; }
+        public string Country { get; set; }
+        public string ImageLink { get; set; }
         public string Wikipedia { get; set; }
 
         public List<Book> Books { get; set; }
