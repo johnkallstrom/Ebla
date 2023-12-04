6000ce4e4089364d78c050850db10f6bd935d537
John Källström <johnkaellstrom@gmail.com>
2023-12-04 13:36:44 +0100

    Add properties to user interface

--- Ebla.Application/Interfaces/Identity/IApplicationUser.cs
+++ Ebla.Application/Interfaces/Identity/IApplicationUser.cs
@@ -2,5 +2,8 @@
 {
     public interface IApplicationUser
     {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
     }
 }
