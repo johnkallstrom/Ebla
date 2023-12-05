4d12a53d22e1346af9bc4c83dfbf5449fb232ddb
John Källström <johnkaellstrom@gmail.com>
2023-12-05 15:05:17 +0100

    Display data in detail pages

--- src/Frontend/Ebla.Web/Features/Book/Details.razor.cs
+++ src/Frontend/Ebla.Web/Features/Book/Details.razor.cs
@@ -2,7 +2,22 @@
 {
     public partial class Details
     {
+        [Inject]
+        public IHttpService HttpService { get; set; }
+
         [Parameter]
         public int BookId { get; set; }
+
+        public BookViewModel Book { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await HttpService.GetAsync($"{Endpoints.Books}/{BookId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Book = await response.Content.ReadFromJsonAsync<BookViewModel>();
+            }
+        }
     }
 }
--- src/Frontend/Ebla.Web/Features/Library/Details.razor.cs
+++ src/Frontend/Ebla.Web/Features/Library/Details.razor.cs
@@ -2,7 +2,22 @@
 {
     public partial class Details
     {
+        [Inject]
+        public IHttpService HttpService { get; set; }
+
         [Parameter]
         public int LibraryId { get; set; }
+
+        public LibraryViewModel Library { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await HttpService.GetAsync($"{Endpoints.Libraries}/{LibraryId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Library = await response.Content.ReadFromJsonAsync<LibraryViewModel>();
+            }
+        }
     }
 }
