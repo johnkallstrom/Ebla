880fc1113c86c7c62a59f8dd158fd27548ea9271
John Källström <johnkaellstrom@gmail.com>
2023-12-05 16:07:42 +0100

    Add genre filter component

---
+++ src/Frontend/Ebla.Web/Features/Book/Components/GenreFilter.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace Ebla.Web.Features.Book.Components
+{
+    public partial class GenreFilter
+    {
+        [Inject]
+        public IHttpService HttpService { get; set; }
+
+        public List<GenreViewModel> Genres { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await HttpService.GetAsync(Endpoints.Genres);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Genres = await response.Content.ReadFromJsonAsync<List<GenreViewModel>>();
+            }
+        }
+    }
+}
