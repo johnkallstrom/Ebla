7d298db2e7f19c12fdba20bb9f785192be4994f9
John Källström <johnkaellstrom@gmail.com>
2023-12-05 15:49:16 +0100

    Endpoints

--- src/Frontend/Ebla.Web/Enums/Endpoints.cs
+++ src/Frontend/Ebla.Web/Enums/Endpoints.cs
@@ -2,11 +2,14 @@
 {
     public static class Endpoints
     {
-        public static readonly string Libraries = "/api/libraries";
-        public static readonly string Books = "/api/books";
         public static readonly string Authors = "/api/authors";
-        public static readonly string Reservations = "/api/reservations";
+        public static readonly string Books = "/api/books";
+        public static readonly string Genres = "/api/genres";
+        public static readonly string Libraries = "/api/libraries";
+        public static readonly string LibraryCards = "/api/libraryCards";
         public static readonly string Loans = "/api/loans";
+        public static readonly string Reservations = "/api/reservations";
+        public static readonly string Reviews = "/api/reviews";
         public static readonly string Users = "/api/users";
         public static readonly string Login = "/api/users/login";
     }
