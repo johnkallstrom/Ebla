c28492b286f951ce8569a5a265f15600c6aa4081
John Källström <johnkaellstrom@gmail.com>
2023-12-06 17:23:39 +0100

    Login form

--- src/Frontend/Ebla.Web/Features/User/Login.razor.cs
+++ src/Frontend/Ebla.Web/Features/User/Login.razor.cs
@@ -5,12 +5,47 @@
         [Inject]
         public IHttpService HttpService { get; set; }
 
-        [Required(ErrorMessage = "Please enter a username")]
-        [DataType(DataType.Text)]
-        public string Username { get; set; }
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
 
-        [Required(ErrorMessage = "Please enter a password")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public LoginViewModel Model { get; set; }
+        public List<string> Errors { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Model = new LoginViewModel();
+            Errors = new List<string>();
+        }
+
+        public async Task Submit()
+        {
+            Errors.Clear();
+
+            try
+            {
+                var httpResponse = await HttpService.PostAsync(Endpoints.Login, Model);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    var result = await httpResponse.Content.ReadFromJsonAsync<Result<string>>();
+
+                    if (result.Succeeded)
+                    {
+                        await LocalStorage.SetItemAsStringAsync("token", result.Data);
+                        NavigationManager.NavigateToAndRefresh(NavigationManager.BaseUri);
+                    }
+                    else
+                    {
+                        Errors.AddRange(result.Errors);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Errors.Add(ex.Message);
+            }
+        }
     }
 }
