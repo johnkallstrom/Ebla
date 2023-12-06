27fb8a0f913c9a5506865f2ac574d4bfd6657c2f
John Källström <johnkaellstrom@gmail.com>
2023-12-06 16:01:40 +0100

    HttpService

--- src/Frontend/Ebla.Web/Services/Interfaces/IHttpService.cs
+++ src/Frontend/Ebla.Web/Services/Interfaces/IHttpService.cs
@@ -4,5 +4,7 @@
     {
         Task<HttpResponseMessage> GetAsync(string uri);
         Task<HttpResponseMessage> PostAsync(string uri, object data);
+        Task<HttpResponseMessage> PutAsync(string uri, object data);
+        Task<HttpResponseMessage> DeleteAsync<T>(string uri, T identifier);
     }
 }
