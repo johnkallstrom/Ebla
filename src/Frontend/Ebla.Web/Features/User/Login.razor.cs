cd6597beedd30070b48be429e352e5bd532cc101
John Källström <johnkaellstrom@gmail.com>
2023-12-06 12:11:34 +0100

    Change

--- src/Frontend/Ebla.Web/Features/User/Login.razor.cs
+++ src/Frontend/Ebla.Web/Features/User/Login.razor.cs
@@ -4,5 +4,13 @@
     {
         [Inject]
         public IHttpService HttpService { get; set; }
+
+        [Required(ErrorMessage = "Please enter a username")]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
