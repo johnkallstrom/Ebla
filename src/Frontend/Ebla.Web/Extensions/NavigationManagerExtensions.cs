e814483aa4ad1807f608d51452b2736f173e4913
John Källström <johnkaellstrom@gmail.com>
2023-12-06 15:46:42 +0100

    Removed extension method

--- src/Frontend/Ebla.Web/Extensions/NavigationManagerExtensions.cs
+++ src/Frontend/Ebla.Web/Extensions/NavigationManagerExtensions.cs
@@ -2,11 +2,6 @@
 {
     public static class NavigationManagerExtensions
     {
-        public static void NavigateToStartPageAndRefresh(this NavigationManager navigationManager)
-        {
-            navigationManager.NavigateTo(navigationManager.BaseUri, true);
-        }
-
         public static void NavigateToAndRefresh(this NavigationManager navigationManager, string uri)
         {
             navigationManager.NavigateTo(uri, true);
