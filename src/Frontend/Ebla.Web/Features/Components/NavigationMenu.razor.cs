0c02b7b1c4fbef388458eced44194a3bd71617f8
John Källström <johnkaellstrom@gmail.com>
2023-12-11 12:52:30 +0100

    Display fullname and role in navmenu

--- src/Frontend/Ebla.Web/Features/Components/NavigationMenu.razor.cs
+++ src/Frontend/Ebla.Web/Features/Components/NavigationMenu.razor.cs
@@ -2,5 +2,19 @@
 {
     public partial class NavigationMenu
     {
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        public string FullName { get; set; } = "Firstname Lastname";
+        public string Role { get; set; } = "Role";
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            FullName = user.GetFullName();
+            Role = user.GetPrimaryRole();
+        }
     }
 }
