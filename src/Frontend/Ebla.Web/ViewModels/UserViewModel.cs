749b49918c66b37cb1cd623ce355bb2f79993130
John Källström <johnkaellstrom@gmail.com>
2023-12-11 11:36:01 +0100

    Update viewmodel

--- src/Frontend/Ebla.Web/ViewModels/UserViewModel.cs
+++ src/Frontend/Ebla.Web/ViewModels/UserViewModel.cs
@@ -4,6 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string[] Roles { get; set; }
     }
