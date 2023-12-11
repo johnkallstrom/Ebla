66d2e6582b439c34b7d774ba1183c9fed92208fb
John Källström <johnkaellstrom@gmail.com>
2023-12-11 15:03:14 +0100

    Property in code behind

--- src/Frontend/Ebla.Web/Features/Index.razor.cs
+++ src/Frontend/Ebla.Web/Features/Index.razor.cs
@@ -2,11 +2,13 @@
 {
     public partial class Index
     {
+        public int TotalBooks { get; set; }
         public double[] PieChartData { get; set; }
         public string[] PieChartLabels { get; set; }
 
         protected override void OnInitialized()
         {
+            TotalBooks = 0;
             PieChartData = new double[] { 50, 35, 15 };
             PieChartLabels = new string[] { "Science Fiction", "Horror", "Non-Fiction" };
         }
