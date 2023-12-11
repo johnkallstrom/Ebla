77c731610a6280a7318804a20769238ca31b18f7
John Källström <johnkaellstrom@gmail.com>
2023-12-11 14:38:54 +0100

    Add new statistics controller

---
+++ src/Backend/Ebla.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,36 @@
+﻿namespace Ebla.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get total amount of books currently in the database
+        /// </summary>
+        /// <returns></returns>
+        [HasReadAccess]
+        [HttpGet("total-books")]
+        public int GetTotalAmountOfBooks()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Get top three genres with most books in percentage
+        /// </summary>
+        /// <returns></returns>
+        [HasReadAccess]
+        [HttpGet("genres")]
+        public double[] GetTop3GenresWithMostBooks()
+        {
+            return [50, 35, 15];
+        }
+    }
+}
