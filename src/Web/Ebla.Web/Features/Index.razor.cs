92bbee9bc1eb05bdc19e5bb54a984133792313c3
John Källström <johnkaellstrom@gmail.com>
2023-12-12 15:58:56 +0100

    Changes

--- src/Web/Ebla.Web/Features/Index.razor.cs
+++ src/Web/Ebla.Web/Features/Index.razor.cs
@@ -8,17 +8,13 @@
         [Inject]
         public IHttpService HttpService { get; set; }
 
-        public StatisticsViewModel Model { get; set; }
+        public StatisticsViewModel Model { get; set; } = new StatisticsViewModel();
         public bool Loading { get; set; } = true;
 
         protected override async Task OnInitializedAsync()
         {
             var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
             var user = authState.User;
-
-            Model = new StatisticsViewModel();
-            Model.GenresWithMostBooksLabels = new string[] { "Genre Label 1", "Genre Label 2", "Genre Label 3" };
-            Model.GenresWithMostBooksPercentages = new double[] { 50, 25, 25 };
 
             if (user.Identity.IsAuthenticated)
             {
