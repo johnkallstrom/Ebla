5cc13a14d59a3655a639282dc02ac0610165206d
John Källström <johnkaellstrom@gmail.com>
2023-12-13 17:27:06 +0100

    Change

--- src/Web/Ebla.Web/ViewModels/StatisticsViewModel.cs
+++ src/Web/Ebla.Web/ViewModels/StatisticsViewModel.cs
@@ -3,13 +3,14 @@
     public class StatisticsViewModel
     {
         public int TotalBooks { get; set; }
+        public Dictionary<string, double> GenreData { get; set; }
         public string[] GenreLabels { get; set; }
-        public double[] GenreData { get; set; }
+        public double[] GenrePercentages { get; set; }
 
         public StatisticsViewModel()
         {
             GenreLabels = new string[] { "Science Fiction", "Horror", "Non-Fiction" };
-            GenreData = new double[] { 40, 35, 25 };
+            GenrePercentages = new double[] { 40, 35, 25 };
         }
     }
 }
