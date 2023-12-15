8fd00e4c9b4168d7d62a66bc39d89314bcf01c24
John Källström <johnkaellstrom@gmail.com>
2023-12-15 17:45:50 +0100

    Update

--- src/Infrastructure/Ebla.Persistence/EntityConfigurations/BookLibraryConfiguration.cs
+++ src/Infrastructure/Ebla.Persistence/EntityConfigurations/BookLibraryConfiguration.cs
@@ -9,16 +9,9 @@
             builder.Property(x => x.BookId).HasColumnName("BookId");
             builder.Property(x => x.LibraryId).HasColumnName("LibraryId");
 
-            builder.HasKey(x => new { x.BookId, x.LibraryId });
-            builder
-                .HasOne(x => x.Book)
-                .WithMany(x => x.BookLibraries)
-                .HasForeignKey(x => x.BookId);
-
-            builder
-                .HasOne(x => x.Library)
-                .WithMany(x => x.BookLibraries)
-                .HasForeignKey(x => x.LibraryId);
+            builder.HasKey(bl => new { bl.BookId, bl.LibraryId });
+            builder.HasOne(bl => bl.Book).WithMany(b => b.BookLibraries).HasForeignKey(bl => bl.BookId);
+            builder.HasOne(bl => bl.Library).WithMany(l => l.BookLibraries).HasForeignKey(bl => bl.LibraryId);
 
             var bookLibraryList = FileManager.ParseJsonFileToList<BookLibrary>("booklibraries.json");
 
