e8033e5188d80faf8f5ec1e757d6085f84a16dd9
John Källström <johnkaellstrom@gmail.com>
2023-12-19 13:35:04 +0100

    Query params

--- src/Web/Ebla.Web/Features/Author/Index.razor.cs
+++ src/Web/Ebla.Web/Features/Author/Index.razor.cs
@@ -6,12 +6,14 @@
         public IHttpService HttpService { get; set; }
 
         public PagedResult<AuthorViewModel> Model { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
 
         protected override async Task OnInitializedAsync()
         {
             try
             {
-                var response = await HttpService.GetAsync(Endpoints.Authors);
+                var response = await HttpService.GetAsync($"{Endpoints.Authors}?pageNumber={PageNumber}&pageSize={PageSize}");
 
                 if (response.IsSuccessStatusCode)
                 {
