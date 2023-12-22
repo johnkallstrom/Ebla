183e7103dde19ef54445765664e0ebef3e27484e
John Källström <johnkaellstrom@gmail.com>
2023-12-22 14:08:14 +0100

    Update vm

--- src/Web/Ebla.Web/ViewModels/StatisticsViewModel.cs
+++ src/Web/Ebla.Web/ViewModels/StatisticsViewModel.cs
@@ -3,6 +3,8 @@
     public class StatisticsViewModel
     {
         public int TotalBooks { get; set; }
+        public int TotalLoans { get; set; }
+        public int TotalReservations { get; set; }
         public string[] GenreLabels { get; set; }
         public double[] GenrePercentages { get; set; }
     }
