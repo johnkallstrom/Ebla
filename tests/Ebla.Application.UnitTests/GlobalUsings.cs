f264b8694033f0a10e09c0277411aef547bca7b3
John Källström <johnkaellstrom@gmail.com>
2024-01-03 15:49:00 +0100

    Usings

--- tests/Ebla.Application.UnitTests/GlobalUsings.cs
+++ tests/Ebla.Application.UnitTests/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using AutoMapper;
 
 global using Ebla.Domain.Entities;
 
+global using Ebla.Application.Common.Exceptions;
 global using Ebla.Application.Interfaces.DataAccess;
 global using Ebla.Application.Authors.Queries;
 global using Ebla.Application.Authors.Responses;
