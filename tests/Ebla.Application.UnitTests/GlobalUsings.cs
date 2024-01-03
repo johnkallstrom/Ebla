e20d159750cf1508229af92e139e171722c29c2b
John Källström <johnkaellstrom@gmail.com>
2024-01-03 09:48:22 +0100

    Add proj references, nuget packages and namespaces

--- tests/Ebla.Application.UnitTests/GlobalUsings.cs
+++ tests/Ebla.Application.UnitTests/GlobalUsings.cs
@@ -1,1 +1,9 @@
 global using Xunit;
+global using Moq;
+global using FluentAssertions;
+
+global using Ebla.Domain.Entities;
+
+global using Ebla.Application.Interfaces.DataAccess;
+global using Ebla.Application.Authors.Queries;
+global using Ebla.Application.Authors.Responses;
