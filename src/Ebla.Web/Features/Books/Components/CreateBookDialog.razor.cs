6c789f630b00082fdc080996cafb09a5c05122b7
John Källström <johnkaellstrom@gmail.com>
2024-01-09 10:12:08 +0100

    Create book form

---
+++ src/Ebla.Web/Features/Books/Components/CreateBookDialog.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace Ebla.Web.Features.Books.Components
+{
+    public partial class CreateBookDialog
+    {
+        [CascadingParameter]
+        public MudDialogInstance MudDialog { get; set; }
+
+        public CreateBookViewModel Model { get; set; } = new CreateBookViewModel();
+
+        private void Cancel() => MudDialog.Cancel();
+
+        private void Submit()
+        {
+            MudDialog.Close(DialogResult.Ok(0));
+        }
+
+        protected override void OnInitialized()
+        {
+            var options = new DialogOptions
+            {
+                Position = DialogPosition.TopCenter,
+                MaxWidth = MaxWidth.Medium,
+                FullWidth = true,
+                CloseOnEscapeKey = true,
+                DisableBackdropClick = true,
+            };
+
+            MudDialog.SetTitle("New book");
+            MudDialog.SetOptions(options);
+        }
+    }
+}
