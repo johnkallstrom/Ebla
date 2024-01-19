87979f0175309d5dfd8266c5c9076d8740e7310f
John Källström <johnkaellstrom@gmail.com>
2024-01-19 13:03:52 +0100

    Add sort by utility class

---
+++ src/Ebla.Web/Features/Books/Utilities/SortBy.cs
@@ -0,0 +1,13 @@
+﻿namespace Ebla.Web.Features.Books.Utilities
+{
+    public static class SortBy
+    {
+        public static Func<BookViewModel, object> Id = (x) => x.Id;
+        public static Func<BookViewModel, object> Title = (x) => x.Title;
+        public static Func<BookViewModel, object> Author = (x) => x.Author;
+        public static Func<BookViewModel, object> Genre = (x) => x.Genre;
+        public static Func<BookViewModel, object> Language = (x) => x.Language;
+        public static Func<BookViewModel, object> Published = (x) => x.Published;
+        public static Func<BookViewModel, object> CreatedOn = (x) => x.CreatedOn;
+    }
+}
