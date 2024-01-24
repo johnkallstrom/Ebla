6da25119200ebe642a7b64e70b2fd3a4ee31331c
John Källström <johnkaellstrom@gmail.com>
2024-01-24 17:27:42 +0100

    Print ids as comma separated string

--- src/Ebla.Application/Books/Commands/DeleteBooks/DeleteBooksCommand.cs
+++ src/Ebla.Application/Books/Commands/DeleteBooks/DeleteBooksCommand.cs
@@ -3,5 +3,17 @@
     public class DeleteBooksCommand : IRequest<Result>
     {
         public int[] Ids { get; set; }
+
+        public override string ToString()
+        {
+            string str = "";
+            Ids.ToList().ForEach(x =>
+            {
+                if (Ids.Last().Equals(x)) str += $"{x}";
+                else str += $"{x}, ";
+            });
+
+            return str;
+        }
     }
 }
