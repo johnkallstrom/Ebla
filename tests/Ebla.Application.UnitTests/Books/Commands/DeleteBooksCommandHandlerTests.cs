323892e43907b5287d3bfce6a51199d747bd11d6
John Källström <johnkaellstrom@gmail.com>
2024-01-24 18:04:01 +0100

    Unit tests

---
+++ tests/Ebla.Application.UnitTests/Books/Commands/DeleteBooksCommandHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Ebla.Application.UnitTests.Books.Commands
+{
+    public class DeleteBooksCommandHandlerTests
+    {
+        private readonly Mock<IBookRepository> _mockBookRepository;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public DeleteBooksCommandHandlerTests()
+        {
+            _mockBookRepository = new Mock<IBookRepository>();
+            _mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task Handle_Should_ThrowNotFoundException_WhenBooksDoNotExist()
+        {
+            var entities = Enumerable.Empty<Book>();
+
+            _mockBookRepository
+                .Setup(x => x.GetBooksAsync(It.IsAny<int[]>()))
+                .ReturnsAsync(entities);
+
+            var request = new DeleteBooksCommand { Ids = [-1, 398, 7310] };
+            var handler = new DeleteBooksCommandHandler(_mockMapper.Object, _mockBookRepository.Object);
+
+            var result = await handler
+                .Invoking(x => x.Handle(request, default))
+                .Should()
+                .ThrowAsync<NotFoundException>();
+        }
+
+        [Fact]
+        public async Task Handle_Should_ThrowNotFoundException_WhenBooksIsNull()
+        {
+            var entities = new List<Book>();
+            entities = null;
+
+            _mockBookRepository
+                .Setup(x => x.GetBooksAsync(It.IsAny<int[]>()))
+                .ReturnsAsync(entities);
+
+            var request = new DeleteBooksCommand { Ids = [-1, 398, 7310] };
+            var handler = new DeleteBooksCommandHandler(_mockMapper.Object, _mockBookRepository.Object);
+
+            var result = await handler
+                .Invoking(x => x.Handle(request, default))
+                .Should()
+                .ThrowAsync<NotFoundException>();
+        }
+    }
+}
