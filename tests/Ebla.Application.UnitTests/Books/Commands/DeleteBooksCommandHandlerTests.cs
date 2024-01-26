2c2a1cd89942247a808116f293e89687e27c1749
John Källström <johnkaellstrom@gmail.com>
2024-01-26 15:40:03 +0100

    Unit tests

--- tests/Ebla.Application.UnitTests/Books/Commands/DeleteBooksCommandHandlerTests.cs
+++ tests/Ebla.Application.UnitTests/Books/Commands/DeleteBooksCommandHandlerTests.cs
@@ -12,6 +12,48 @@
         }
 
         [Fact]
+        public async Task Handle_Should_InvokeGetBooksAsyncOnce_WhenRequestIsValidAndBooksExist()
+        {
+            // Arrange
+            var books = GetBooks();
+
+            _mockBookRepository
+                .Setup(x => x.GetBooksAsync(It.IsAny<int[]>()))
+                .ReturnsAsync(books);
+
+            var request = new DeleteBooksCommand { Ids = [1] };
+            var handler = new DeleteBooksCommandHandler(_mockMapper.Object, _mockBookRepository.Object);
+
+            // Act
+            var result = await handler.Handle(request, default);
+
+            // Assert
+            _mockBookRepository.Verify(x => x.GetBooksAsync(request.Ids), Times.Once);
+            result.Succeeded.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Handle_Should_InvokeDeleteBooksOnce_WhenRequestIsValidAndBooksExist()
+        {
+            // Arrange
+            var books = GetBooks();
+
+            _mockBookRepository
+                .Setup(x => x.GetBooksAsync(It.IsAny<int[]>()))
+                .ReturnsAsync(books);
+
+            var request = new DeleteBooksCommand { Ids = [1] };
+            var handler = new DeleteBooksCommandHandler(_mockMapper.Object, _mockBookRepository.Object);
+
+            // Act
+            var result = await handler.Handle(request, default);
+
+            // Assert
+            _mockBookRepository.Verify(x => x.DeleteBooks(books), Times.Once);
+            result.Succeeded.Should().BeTrue();
+        }
+
+        [Fact]
         public async Task Handle_Should_ThrowNotFoundException_WhenBooksDoNotExist()
         {
             var entities = Enumerable.Empty<Book>();
@@ -51,5 +93,28 @@
                 .ThrowAsync<NotFoundException>()
                 .WithMessage(expectedMsg);
         }
+
+        #region Helpers
+        private List<Book> GetBooks()
+        {
+            var entities = new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "Rendezvous with Rama",
+                    Pages = 256,
+                    Published = new DateTime(1973, 6, 1),
+                    Language = "English",
+                    Country = "United Kingdom",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/e/e1/Rama_copy.jpg",
+                    AuthorId = 1,
+                    GenreId = 1
+                }
+            };
+
+            return entities;
+        }
+        #endregion
     }
 }
