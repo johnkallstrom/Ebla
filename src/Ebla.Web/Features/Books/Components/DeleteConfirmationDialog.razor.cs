3872fa0e1b8e1d5bfec8b12f3f303cc7ce5fed0d
John Källström <johnkaellstrom@gmail.com>
2024-01-30 15:58:12 +0100

    Pass vm to api call

--- src/Ebla.Web/Features/Books/Components/DeleteConfirmationDialog.razor.cs
+++ src/Ebla.Web/Features/Books/Components/DeleteConfirmationDialog.razor.cs
@@ -9,14 +9,19 @@
         public MudDialogInstance MudDialog { get; set; }
 
         [Parameter]
-        public int[] Ids { get; set; }
+        public HashSet<BookViewModel> BooksToDelete { get; set; }
 
         [Parameter]
         public string Text { get; set; }
 
         private async Task DeleteBooksAsync()
         {
-            var response = await HttpService.DeleteAsync($"{Endpoints.Books}/delete", Ids);
+            var model = new DeleteBooksViewModel
+            {
+                Ids = BooksToDelete?.Select(x => x.Id).ToArray()
+            };
+
+            var response = await HttpService.DeleteAsync($"{Endpoints.Books}/delete", model);
             if (response.Succeeded)
             {
                 MudDialog.Close(DialogResult.Ok(true));
