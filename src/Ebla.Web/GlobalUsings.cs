aecab83f26eecbb80b8a858205763a305c9c6375
John Källström <johnkaellstrom@gmail.com>
2024-01-30 15:03:38 +0100

    Add Newtonsoft.Json to web project

--- src/Ebla.Web/GlobalUsings.cs
+++ src/Ebla.Web/GlobalUsings.cs
@@ -27,3 +27,5 @@ global using System.IdentityModel.Tokens.Jwt;
 global using System.ComponentModel.DataAnnotations;
 global using System.Net.Http.Json;
 global using System.Net.Http.Headers;
+global using Newtonsoft.Json;
+global using System.Text;
