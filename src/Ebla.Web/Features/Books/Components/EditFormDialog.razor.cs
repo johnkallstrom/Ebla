332d1e75b06b662256ac3eef6ba47026dea6f250
John Källström <johnkaellstrom@gmail.com>
2024-02-07 16:05:21 +0100

    Edit form dialog and vm

---
+++ src/Ebla.Web/Features/Books/Components/EditFormDialog.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace Ebla.Web.Features.Books.Components
+{
+    public partial class EditFormDialog
+    {
+        [CascadingParameter]
+        public MudDialogInstance MudDialog { get; set; }
+
+        [Inject]
+        public IHttpService<Response> HttpService { get; set; }
+
+        public EditBookViewModel Model { get; set; } = new EditBookViewModel();
+
+        private async Task Submit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
