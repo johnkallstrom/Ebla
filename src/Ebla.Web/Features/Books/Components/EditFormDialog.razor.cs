5ecfa72b143b3a8751487981ae800026b58b5328
John Källström <johnkaellstrom@gmail.com>
2024-02-09 11:48:58 +0100

    Call api and map response to edit model

--- src/Ebla.Web/Features/Books/Components/EditFormDialog.razor.cs
+++ src/Ebla.Web/Features/Books/Components/EditFormDialog.razor.cs
@@ -8,11 +8,43 @@
         [Inject]
         public IHttpService<Response> HttpService { get; set; }
 
+        [Inject]
+        public IBookHttpService BookHttpService { get; set; }
+
+        [Parameter]
+        public int? BookId { get; set; }
+
         public EditBookViewModel Model { get; set; } = new EditBookViewModel();
 
-        private async Task Submit()
+		protected override async Task OnInitializedAsync()
+		{
+            if (BookId.HasValue)
+            {
+				var book = await BookHttpService.GetAsync(BookId.Value);
+				if (book is not null)
+				{
+					Model = ToEditBookViewModel(book);
+				}
+			}
+		}
+
+		private async Task Submit()
         {
-            throw new NotImplementedException();
+        }
+
+        private EditBookViewModel ToEditBookViewModel(BookViewModel book)
+        {
+            return new EditBookViewModel
+            {
+				Id = book.Id,
+			    Title = book.Title,
+			    Description = book.Description,
+			    Pages = book.Pages,
+			    Published = book.Published,
+			    Language = book.Language,
+			    Country = book.Country,
+			    Image = book.Image
+		    };
         }
     }
 }
